ec57846f32a2d103f11a7eaf159d9d9176bd149a
Francisco Gemaque <gemaquejr@hotmail.com>
2024-12-15 22:05:25 -0300

    Fix: engine.js e implementa função drawCards
    Signed-off-by: Francisco Gemaque <gemaquejr@hotmail.com>

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -19,11 +19,11 @@ const state = {
 };
 
 const playerSides = {
-    player1: "player-field-card",
-    computer: "computer-field-card",
+    player1: "player-cards",
+    computer: "computer-cards",
 }
 
-const pathImages = ".src/assets/icons/";
+const pathImages = "./src/assets/icons/";
 const cardData = [
     {
         id:0,
@@ -51,12 +51,37 @@ const cardData = [
     }
 ];
 
+async function getRandomCardId() {
+    const randomIndex = Math.floor(Math.random() * cardData.length);
+    return cardData[randomIndex].id;
+}
+
+async function createCardImage(IdCard, fieldSide) {
+    const cardImage = document.createElement("img");
+    cardImage.setAttribute("height", "100px");
+    cardImage.setAttribute("src", "./src/assets/icons/card-back.png");
+    cardImage.setAttribute("data-id", IdCard);
+    cardImage.classList.add("card");
+
+    if (fieldSide === playerSides.player1) {
+        cardImage.addEventListener("click", () => {
+            setCardsField(cardImage.getAttribute("data-id"));
+        });
+    }
+
+    cardImage.addEventListener("mouseover", () => {
+        drawSelectCard(IdCard);
+    });
+
+    return cardImage;
+}
+
 async function drawCards(cardNumbers, fieldSide) {
     for(let i = 0; i < cardNumbers; i++) {
         const randomIdCard = await getRandomCardId();
-        const cardImag = await createCardImage(randomIdCard, fieldSide);
+        const cardImage = await createCardImage(randomIdCard, fieldSide);
 
-        document.getElementById(fieldSide).appendChild(cardImag);
+        document.getElementById(fieldSide).appendChild(cardImage);
     }    
 }
 
